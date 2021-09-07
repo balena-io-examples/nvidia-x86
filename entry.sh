5e22f027e83cac525b4ad41f9b86a6302c3ff841
Alan Boris <39385685+alanb128@users.noreply.github.com>
2021-09-07 11:14:59 -0400

    Create entry.sh

---
+++ entry.sh
@@ -0,0 +1,6 @@
+sudo insmod /nvidia/driver/nvidia.ko
+sudo insmod /nvidia/driver/nvidia-modeset.ko
+sudo insmod /nvidia/driver/nvidia-uvm.ko
+sudo /usr/bin/nvidia-smi
+/usr/bin/nvidia-smi
+sudo nvidia-modprobe
