c164906c15f211e2e4a24ce99c5f2942fa0f075f
Alan Boris <39385685+alanb128@users.noreply.github.com>
2021-11-09 00:49:11 -0500

    Remove Nouveau modules

--- entry.sh
+++ entry.sh
@@ -1,3 +1,10 @@
+# Remove Nouveau modules
+echo 0 > /sys/class/vtconsole/vtcon1/bind
+sleep 2
+rmmod nouveau
+sleep 2
+
+# Insert Nvidia modules
 insmod /nvidia/driver/nvidia.ko
 insmod /nvidia/driver/nvidia-modeset.ko
 insmod /nvidia/driver/nvidia-uvm.ko
