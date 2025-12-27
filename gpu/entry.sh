37042378834c7c8bb0f18784ece0ab36123cd4fe
Alan Boris <39385685+alanb128@users.noreply.github.com>
2025-12-26 21:38:21 -0500

    Update examples and remove balenalib base images
    Change-type: patch
    Signed-off-by: Alan Boris alan@balena.io

--- gpu/entry.sh
+++ gpu/entry.sh
@@ -1,3 +1,15 @@
+
+# Stop Plymouth on the host OS (because it's displaying the balena logo)
+# See https://forums.balena.io/t/blacklist-drivers-in-host-os/163437/25
+DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket \
+  dbus-send \
+  --system \
+  --print-reply \
+  --dest=org.freedesktop.systemd1 \
+  /org/freedesktop/systemd1 \
+  org.freedesktop.systemd1.Manager.StopUnit \
+  string:plymouth-start.service string:replace
+
 # Remove Nouveau modules
 sleep 6
 echo 0 > /sys/class/vtconsole/vtcon1/bind
@@ -5,7 +17,21 @@ sleep 2
 rmmod nouveau
 sleep 2
 
+# unload nvidiafb - it's a driver that gets loaded for some, but not all cards.
+rmmod nvidiafb
+sleep 2
+
 # see https://forums.balena.io/t/blacklist-drivers-in-host-os/163437/25
+
+# For loading firmware into the card
+# (-n switch below prevents a newline from being added)
+
+echo -n "/run/mount" > /sys/module/firmware_class/parameters/path
+
+# ***** Make sure to change the line below to match your driver version in the Dockerfile! *****
+
+cp /usr/src/nvidia/NVIDIA-Linux-x86_64-580.95.05/firmware/gsp_ga10x.bin /data
+
 
 # Insert Nvidia modules
 insmod /nvidia/driver/nvidia.ko
